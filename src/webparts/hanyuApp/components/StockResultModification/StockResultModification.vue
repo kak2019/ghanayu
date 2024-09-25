8fe5cdd23950dc38809fc0c21145c9c50ddd8fe8
Fei Katherine <katherine.fei@consultant.udtrucks.com>
2024-09-25 16:41:25 +0800

    Bug 301070: [Hanyu-在庫&実績修正] In the downloaded excel , the format of "修正年月日" are not correct

--- src/webparts/hanyuApp/components/StockResultModification/StockResultModification.vue
+++ src/webparts/hanyuApp/components/StockResultModification/StockResultModification.vue
@@ -506,7 +506,7 @@ export default {
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
       const year = date.getFullYear();
-      return `${month}/${day}/${year}`;
+      return `${year}/${month}/${day}`;
     },
 
     addOneDay(date) {
